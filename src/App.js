b01239cb1471e0a0c8d0dee4228cc9094d8391bb
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-05 17:15:52 +0100

    Create React app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>It works?</h1>
+      <Button variant="secondary">Secondary</Button>
+    </div>
+  );
+}
+
+export default App;
