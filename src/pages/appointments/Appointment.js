624791977bc26132cd81f31c0406a733554aee50
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-18 18:36:03 +0100

    Style appointment page and my appointments page

--- src/pages/appointments/Appointment.js
+++ src/pages/appointments/Appointment.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-import styles from '../../styles/Appointment.module.css'
-import { Card } from 'react-bootstrap'
+import styles from '../../styles/Appointment.module.css';
+import { Card } from 'react-bootstrap';
 
 const Appointment = (props) => {
 
@@ -9,22 +9,42 @@ const Appointment = (props) => {
         treatment, date, time, status, notes, appointmentPage,
     } = props
 
-    const displayTime = time/100 + ":" + (time) % 100/50 *3 + '0'
+    const displayTime = time / 100 + ":" + (time) % 100 / 50 * 3 + '0'
 
     return (
-        <Card className={status==="Upcoming" ? (styles.UpcomingAppointmentCard) : (styles.PastAppointmentCard)}>
-            <Card.Body>
-                <div className={styles.CardHeader}>
-                    {treatment && <Card.Title className="text-center">My &nbsp;{treatment}</Card.Title>}
-                    {appointmentPage && status === "Upcoming" && "..."}
-                </div>
+        appointmentPage ? (
+            <>
+            <div className={styles.AppointmentDetail}>
+                <h1>{treatment}</h1> 
+                {status === "Upcoming" && <span>
+                <i className="fas fa-edit"></i>
+                <i className="fas fa-trash"></i></span>}
+                <hr />
+                <p>Date: {date}</p>
+                <p>Time: {time}</p>
+                {notes && <p>{notes}</p>}
+                <hr />
+            </div>
+            <div className={styles.AppointmentPageHelp}>
+                <p>Need help with this appointment?<br />
+                Call us or send us an email.</p>
+            </div>
+            </>
+        ) : (
+            <>
+                <Card className={styles.AppointmentCard} >
+                    <Card.Body>
+                        <div className={styles.CardHeader}>
+                            {treatment && <Card.Title className="text-center">My &nbsp;{treatment}</Card.Title>}
 
-                {date && time && <Card.Text>{date} at {displayTime} </Card.Text>}
+                        </div>
 
-                {notes && <Card.Text className="text-center">{notes}</Card.Text>}
-            </Card.Body>
+                        {date && time && <Card.Text>{date} at {displayTime} </Card.Text>}
 
-        </Card>
+                        {notes && <Card.Text className="text-center">{notes}</Card.Text>}
+                    </Card.Body>
+                </Card></>
+        )
     )
 }
 
--- src/pages/appointments/AppointmentPage.js
+++ src/pages/appointments/AppointmentPage.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 
 import { axiosReq } from '../../api/axiosDefaults'
 import Appointment from './Appointment';
+import Asset from '../../components/Asset';
 
 
 function AppointmentPage() {
@@ -11,25 +12,30 @@ function AppointmentPage() {
     const {id} = useParams();
 
     const [appointment, setAppointment] = useState({results: []});
+    const [hasLoaded, setHasLoaded] = useState(false);
 
     useEffect(() => {
-        const handleMount = async () => {
+        const fetchAppointment = async () => {
             try {
                 const { data } = await axiosReq.get(`/my-appointments/${id}`);
                 setAppointment({results: [data]});
-
+                setHasLoaded(true);
             } catch(err) {
                 console.log(err);
             }
         };
-
-        handleMount();
+        setHasLoaded(false);
+        fetchAppointment();
     }, [id]);
 
 
   return (
     <section className={styles.OffsetTop}>
-      <Appointment {...appointment.results[0]} setAppointments={setAppointment} appointmentPage />
+      {hasLoaded ? (
+        <Appointment {...appointment.results[0]} appointmentPage />
+      ) : (
+        <Asset spinner />
+      )}
     </section>
   )
 }
