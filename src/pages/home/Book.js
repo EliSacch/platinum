c5b28de56d9a41991bc82a8663c4050cef633145
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-22 14:01:32 +0100

    Add booking form to home page for logged in users

--- src/pages/home/Book.js
+++ src/pages/home/Book.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import styles from '../../styles/Book.module.css'
+import React from 'react';
+import styles from '../../styles/Book.module.css';
 
-function Book() {
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import AppointmentsCreateForm from '../appointments/AppointmentsCreateForm';
+
+const Book = () => {
+  const currentUser = useCurrentUser();
+
   return (
-    <section className={styles.Section}>
-      <span id="book" className={`${styles.NavOffset} "d-none"`}></span>
+
+    currentUser ? (
+      <div className={styles.BookingForm}>
+        <AppointmentsCreateForm />
+      </div>
+    ) : (
+
+      <section className={styles.Section} >
+        <span id="book" className={`${styles.NavOffset} "d-none"`}></span>
         <h2>Book with us</h2>
         <p>You can <a href="tel:353-123-4567">call us</a> or send us an <a href="mailto:platinum@booking.com">
-                email </a>
-        , and we will be happy to book you in.</p>
+          email </a>
+          , and we will be happy to book you in.</p>
         <p>Sign up or sign in to your account to book directly.</p>
-    </section>
+      </section >
+
+    )
   )
 }
 
 export default Book
+
