21400e8bf969c01aa966250daf5c39806da040c0
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-23 11:57:27 +0100

    Create the staff appointments edit form

--- src/pages/staff/StaffAppointmentEditForm.js
+++ src/pages/staff/StaffAppointmentEditForm.js
@@ -1,72 +1,50 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
+// custom function to calculate time slots
+import calculateTimeSlots from "../../utils/calculateTimeSlots";
 // bootstrap
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import { Alert } from "react-bootstrap";
 // custom componenets
-import Asset from '../../components/Asset';
+import Asset from "../../components/Asset";
 // custom css
-import styles from "../../styles/Treatments.module.css";
+import styles from "../../styles/AppointmentsList.module.css";
 
 
-function TreatmentEditForm({ setShow, query, setQuery, editId }) {
+function StaffAppointmentEditForm({ setShow, query, setQuery, editId }) {
 
-    const [treatmentData, setTreatmentData] = useState({
-        title: "",
-        description: "",
-        price: 0,
-        duration: 50,
-        image: "",
-        is_active: false,
+    const [appointmentData, setAppointmentData] = useState({
+        owner: "",
+        client_name: "",
+        date: "",
+        time: 900,
+        treatment: "Consultation",
+        notes: "",
     });
 
-    const { title, description, price, duration, image, is_active } = treatmentData;
-    const imageInput = useRef(null);
+    const { owner, client_name, date, time, treatment, notes } = appointmentData;
 
-    // Set the minimum and max duration
-    const min_duration = 50;
-    const max_duration = 450;
-    // determine the range
-    const range = (max_duration - min_duration) / 50
-
-    const selectOptions = (range) => {
-        // get the range
-        const options = [...Array(range).keys()];
-
-        // create a new array from the range, to get the valid time values in the range
-        const slots = options.map(option => (option + 1) * 50);
-        const labels = [];
-        for (let slot of slots) {
-            let label = slot % 100 === 0 ? (
-                `${slot / 100}:00`
-            ) : (
-                `${(slot - (slot % 100)) / 100}:30`
-            );
-            labels.push(label);
-        }
-
-        // create the final choices to display, based on the slots
-        const choices = slots.map((slot, i) => (
-            { value: slot, label: labels[i] }
-        ));
-        return choices
-    }
+    // set the clients that will be displayed as options to select
+    const [clients, setClients] = useState({ results: [] });
+    // set the treatments that will be displayed as options to select
+    const [treatments, setTreatments] = useState({ results: [] });
 
     const [errors, setErrors] = useState({});
     const [hasLoaded, setHasLoaded] = useState(true);
+
 
     useEffect(() => {
         const initializeForm = async () => {
             try {
                 /**
-                 * We try to initialize the form with the treatment data.
+                 * We try to initialize the form with the appointment data.
                  */
-                const { data } = await axiosReq.get(`/treatments/${editId}/`);
-                const { title, description, price, duration, image, is_active } = data;
-                setTreatmentData(
-                    { title, description, price, duration, image, is_active }
+                const { data } = await axiosReq.get(`/appointments/${editId}/`);
+                const { owner, client_name, date, time, treatment, notes } = data;
+                setAppointmentData(
+                    { owner, client_name, date, time, treatment, notes }
                 )
                 setHasLoaded(true);
             } catch (err) {
@@ -80,9 +58,9 @@ function TreatmentEditForm({ setShow, query, setQuery, editId }) {
     }, [editId]);
 
     /**
-     * When the create treatment form is submitted, the data
+     * When the create appointment form is submitted, the data
      * is validated in the backend. If the data is valid,
-     * the new treatment is added to the database and the
+     * the new appointment is added to the database and the
      * query is refreshed so that it appears in the list already
      * @param {submit} event 
      */
@@ -91,16 +69,16 @@ function TreatmentEditForm({ setShow, query, setQuery, editId }) {
 
         const formData = new FormData();
 
-        formData.append("title", title);
-        formData.append("description", description);
-        formData.append("price", price);
-        formData.append("duration", duration);
-        formData.append("image", imageInput.current.files[0]);
-        formData.append("is_active", is_active);
+        formData.append("owner", owner);
+        formData.append("client_name", client_name);
+        formData.append("date", date);
+        formData.append("time", time);
+        formData.append("treatment", treatment);
+        formData.append("notes", notes);
 
         try {
-            setHasLoaded(false);
-            await axiosReq.put(`/treatments/${editId}/`, formData);
+            setHasLoaded(false)
+            await axiosReq.post(`/appointments/${editId}`, formData);
             setShow(false);
             /* To refresh the query we make sure that 
             we set it to something different than it's initial value */
@@ -119,135 +97,173 @@ function TreatmentEditForm({ setShow, query, setQuery, editId }) {
      * @param {change} event 
      */
     const handleChange = (event) => {
-        setTreatmentData({
-            ...treatmentData,
-            [event.target.name]:
-                // for checkboxes we use the checked status instead of value
-                event.target.type === 'checkbox'
-                    ? event.target.checked
-                    : event.target.value,
+        setAppointmentData({
+            ...appointmentData,
+            [event.target.name]: event.target.value,
         });
     };
 
-    /**
-     * Set the handleChange function for controlled image upload field
-     * @param {change} event 
-     */
-    const handleChangeImage = (event) => {
-        if (event.target.files.length) {
-            URL.revokeObjectURL(image);
-            setTreatmentData({
-                ...treatmentData,
-                image: URL.createObjectURL(event.target.files[0]),
-            });
-        }
-    };
+    useEffect(() => {
+        /**
+         * This functions retrieves the clients,
+         * so that they can be displayed in the form.
+         */
+        const fetchClients = async () => {
+            try {
+                const { data } = await axiosReq.get('/clients/');
+                setClients(data);
+
+            } catch (err) {
+                console.log(err, 'from fetchClients');
+            }
+        };
+        /**
+         * This functions retrieves the active treatments,
+         * so that they can be displayed in the form.
+         */
+        const fetchTreatments = async () => {
+            try {
+                const { data } = await axiosReq.get('/treatments/');
+                setTreatments(data);
+            } catch (err) {
+                console.log(err, 'from fetchTreatment');
+            }
+        };
+        fetchClients();
+        fetchTreatments();
+    }, [setClients])
 
     const textFields = (
         <div className="text-center">
-            {/* Text form to enter the treatment title */}
-            <Form.Group controlId="title" className={styles.Input}>
-                <Form.Label className={styles.InputLabel}>Title</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="title"
-                    name="title"
-                    value={title}
-                    onChange={handleChange}
-                />
-            </Form.Group>
-            {errors.title?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                    {message}
-                </Alert>
-            ))}
-
-            {/* Text area form to enter the dreatment description (not mandatory) */}
-            <Form.Group controlId="description" className={styles.Input}>
-                <Form.Label className={styles.InputLabel}>Description</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="description"
-                    name="description"
-                    value={description}
-                    onChange={handleChange}
-                />
-            </Form.Group>
-            {errors.description?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                    {message}
-                </Alert>
-            ))}
-
-            {/* Number field to enter the treatment price */}
-            <Form.Group controlId="price" className={styles.Input}>
-                <Form.Label className={styles.InputLabel}>Price</Form.Label>
-                <Form.Control
-                    type="number"
-                    placeholder="price"
-                    name="price"
-                    value={price}
-                    onChange={handleChange}
-                    min='0'
-                />
-            </Form.Group>
-            {errors.price?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                    {message}
-                </Alert>
-            ))}
-
-            {/* Select form to pick the treatment duration.
-             Users can select from slots of 30 minutes */}
-            <Form.Group className={styles.Input} controlId="duration">
-                <Form.Label className={styles.InputLabel}>Duration</Form.Label>
+            {/* client is a select form that takes the clients as options */}
+            <Form.Group className={styles.Input} controlId="owner">
+                <Form.Label className={styles.SelectInputLabel}>Client</Form.Label>
                 <Form.Control
                     as="select"
-                    name="duration"
-                    value={duration}
+                    name="owner"
+                    value={owner}
+                    onChange={handleChange}
+                >
+                    <option key={-1} value={null}> ----- </option>
+                    {
+                        clients.results.map((c, i) => (
+                            <option
+                                key={i}
+                                value={c.owner}>
+                                {c.owner}
+                            </option>
+                        )
+                        )
+                    }
+                </Form.Control>
+            </Form.Group>
+            {errors.owner?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            {/* Text area form to enter the client_name 
+            (mandatory only if the selected owner is null) */}
+            <Form.Group controlId="client_name" className={styles.Input}>
+                <Form.Label className={styles.InputLabel}>Client name</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="enter the client name"
+                    name="client_name"
+                    value={client_name}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors.client_name?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            {/* Date picker to select the appointment date */}
+            <Form.Group className={styles.Input} controlId="date">
+                <Form.Label className={styles.SelectInputLabel}>Date</Form.Label>
+                <Form.Control
+                    type="date"
+                    name="date"
+                    value={date}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors.date?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            {/* Select form to pick the appointment time.
+             Users can select from slots of 30 minutes */}
+            <Form.Group className={styles.Input} controlId="time">
+                <Form.Label className={styles.SelectInputLabel}>Time</Form.Label>
+                <Form.Control
+                    as="select"
+                    name="time"
+                    value={time}
                     onChange={handleChange}
                 >
                     {
                         // Get the array of option and display an <option> element for each one
-                        selectOptions(range).map(option => (
-                            <option value={option.value}>{option.label}</option>
+                        calculateTimeSlots(900, 1700, 850).map(option => (
+                            <option value={option.value} key={option.value}>{option.label}</option>
                         ))
                     }
                 </Form.Control>
             </Form.Group>
-            {errors.duration?.map((message, idx) => (
+            {errors.time?.map((message, idx) => (
                 <Alert variant="warning" key={idx}>
                     {message}
                 </Alert>
             ))}
 
-            {/* File upload component */}
-            <Form.Group className={styles.FileInput} controlId="image">
-                <Form.Label className={styles.InputLabel}>Image</Form.Label>
-                <Form.File
-                    accept="image/*"
-                    onChange={handleChangeImage}
-                    ref={imageInput}
-                />
-            </Form.Group>
-            {errors.image?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                    {message}
-                </Alert>
-            ))}
-
-            {/* Checkbox to set the treatment status is_active */}
-            <Form.Group controlId="is_active">
-                <Form.Check
-                    type="checkbox"
-                    label="Active"
-                    name="is_active"
-                    checked={is_active}
+            {/* treatment is a select form that takes the treatments as options */}
+            <Form.Group className={styles.Input} controlId="treatment">
+                <Form.Label className={styles.SelectInputLabel}>Treatment</Form.Label>
+                <Form.Control
+                    as="select"
+                    name="treatment"
+                    value={treatment}
                     onChange={handleChange}
-                    className={styles.Checkbox}
+                >
+                    {
+                        // filter the result to get only active treatments
+                        treatments.results.filter(
+                            res => res.is_active===true
+                        ).map((t, i) => (
+                            <option
+                                key={i}
+                                value={t.title}>
+                                {t.title}
+                            </option>
+                        )
+                        )
+                    }
+                </Form.Control>
+            </Form.Group>
+            {errors.treatment?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            {/* Text area input to add notes to the appointment*/}
+            <Form.Group controlId="notes" className={styles.Input}>
+                <Form.Label className={styles.SelectInputLabel}>Notes</Form.Label>
+                <Form.Control
+                    as="textarea" rows={5}
+                    type="text"
+                    placeholder="notes"
+                    name="notes"
+                    value={notes}
+                    onChange={handleChange}
                 />
             </Form.Group>
-            {errors.is_active?.map((message, idx) => (
+            {errors.notes?.map((message, idx) => (
                 <Alert variant="warning" key={idx}>
                     {message}
                 </Alert>
@@ -255,7 +271,7 @@ function TreatmentEditForm({ setShow, query, setQuery, editId }) {
 
             {/* Form action buttons */}
             <Button className={styles.AddNewBtn} type="submit">
-                Save
+                create
             </Button>
             {errors.non_field_errors?.map((message, idx) => (
                 <Alert key={idx} variant="warning" className="mt-3">
@@ -267,18 +283,20 @@ function TreatmentEditForm({ setShow, query, setQuery, editId }) {
 
     return (
         <section className={styles.OffsetTop}>
-            {hasLoaded ? (
-                <Form
-                    onSubmit={handleSubmit}
-                    className={styles.Form}
-                >
-                    <Container>{textFields}</Container>
-                </Form>
-            ) : (
-                <Asset spinner />
-            )}
+            {
+                hasLoaded ? (
+                    <Form
+                        onSubmit={handleSubmit}
+                        className={styles.Form}
+                    >
+                        <Container>{textFields}</Container>
+                    </Form>
+                ) : (
+                    <Asset spinner />
+                )
+            }
         </section>
     );
 }
 
-export default TreatmentEditForm;
+export default StaffAppointmentEditForm;
