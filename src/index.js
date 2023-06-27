789d45dca075447a3a6da437cd78692048cb1733
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-27 21:41:58 +0100

    Fix issue with node packages that broke everything

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
